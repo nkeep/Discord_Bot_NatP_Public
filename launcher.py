58dfc81a5f3964558485af4e45050c9eae4e1e62
Nathan Keep <ntkeep@gmail.com>
2021-12-29 21:13:33 -0600

    add memetext command

--- launcher.py
+++ launcher.py
@@ -1,9 +1,10 @@
 from lib.bot import bot
 
-VERSION = "1.3"
+VERSION = "1.4"
 
 bot.run(VERSION)
 
 #Version Notes
 #1.2    Add random funny command
 #1.3    Fix dblist command for more than 2k chars
+#1.4    Add memetext command for Joe
--- lib/cogs/meme.py
+++ lib/cogs/meme.py
@@ -4,10 +4,14 @@ from discord import File
 from difflib import get_close_matches
 from typing import Optional
 from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw
 import deeppyer
 import requests
-import uuid
 import shutil
+import uuid
+import math
+import sys
 import os
 
 from ..db import db
@@ -62,6 +66,30 @@ class Meme(Cog):
                         img = await deeppyer.deepfry(img, flares=False)
                         img.save(files+"deepfry.jpg")
                         await ctx.send(file=File(files+"deepfry.jpg"))
+                    except Exception as e:
+                        print(e)
+        except:
+            await ctx.send("Must contain an image upload")
+
+    @command(name="memetext")
+    async def memetext(self, ctx, *, text):
+        text = text.split(";")
+        print(text[0], text[1])
+        try:
+            url = ctx.message.attachments[0].url
+            if url[0:26] == "https://cdn.discordapp.com":
+                r = requests.get(url, stream=True)
+                filetype = "." + str(url.split('.')[-1])
+
+                with open(files + "memetext" + filetype, 'wb') as out_file:
+                    try:
+                        shutil.copyfileobj(r.raw, out_file)
+                        img = Image.open(files+"memetext" + filetype)
+                        draw = ImageDraw.Draw(img)
+                        img, draw = drawText(img, draw, text[0], "top")
+                        img, draw = drawText(img, draw, text[1], "bottom")
+                        img.save(files+"memetext" + filetype)
+                        await ctx.send(file=File(files+"memetext" + filetype))
                     except Exception as e:
                         print(e)
         except:
@@ -159,3 +187,101 @@ class Meme(Cog):
 
 def setup(bot):
 	bot.add_cog(Meme(bot))
+
+#Used this guy's code: https://github.com/lipsumar/meme-caption/blob/master/meme.py
+def drawText(img, draw, msg, pos):
+    fontSize = math.floor(img.width / 8)
+    lines = []
+
+    font = ImageFont.truetype(files + "impact.ttf", fontSize)
+    w, h = draw.textsize(msg, font)
+
+    imgWidthWithPadding = img.width * 0.99
+
+    # 1. how many lines for the msg to fit ?
+    lineCount = 1
+    if(w > imgWidthWithPadding):
+        lineCount = int(round((w / imgWidthWithPadding) + 1))
+
+    if lineCount > 2:
+        while 1:
+            fontSize -= 2
+            font = ImageFont.truetype("impact.ttf", fontSize)
+            w, h = draw.textsize(msg, font)
+            lineCount = int(round((w / imgWidthWithPadding) + 1))
+            print("try again with fontSize={} => {}".format(fontSize, lineCount))
+            if lineCount < 3 or fontSize < 10:
+                break
+
+    print("fontsize: {}".format(fontSize))
+
+    print("img.width: {}, text width: {}".format(img.width, w))
+    print("Text length: {}".format(len(msg)))
+    print("Lines: {}".format(lineCount))
+
+
+    # 2. divide text in X lines
+    lastCut = 0
+    isLast = False
+    for i in range(0,lineCount):
+        if lastCut == 0:
+            cut = (len(msg) / lineCount) * i
+        else:
+            cut = lastCut
+
+        if i < lineCount-1:
+            nextCut = int((len(msg) / lineCount) * (i+1))
+        else:
+            nextCut = len(msg)
+            isLast = True
+
+        print("cut: {} -> {}".format(cut, nextCut))
+
+        # make sure we don't cut words in half
+        if nextCut == len(msg) or msg[nextCut] == " ":
+            print("may cut")
+        else:
+            print("may not cut")
+            while msg[nextCut] != " ":
+                nextCut += 1
+            print("new cut: {}".format(nextCut))
+
+        print(int(cut), nextCut)
+        line = msg[int(cut):nextCut].strip()
+
+        # is line still fitting ?
+        w, h = draw.textsize(line, font)
+        if not isLast and w > imgWidthWithPadding:
+            print("overshot")
+            nextCut -= 1
+            while msg[nextCut] != " ":
+                nextCut -= 1
+            print("new cut: {}".format(nextCut))
+
+        lastCut = nextCut
+        lines.append(msg[int(cut):nextCut].strip())
+
+    print(lines)
+
+    # 3. print each line centered
+    lastY = -h
+    if pos == "bottom":
+        lastY = img.height - h * (lineCount+1) - 10
+
+    for i in range(0,lineCount):
+        w, h = draw.textsize(lines[i], font)
+        textX = img.width/2 - w/2
+        #if pos == "top":
+        #    textY = h * i
+        #else:
+        #    textY = img.height - h * i
+        textY = lastY + h
+        draw.text((textX-2, textY-2),lines[i],(0,0,0),font=font)
+        draw.text((textX+2, textY-2),lines[i],(0,0,0),font=font)
+        draw.text((textX+2, textY+2),lines[i],(0,0,0),font=font)
+        draw.text((textX-2, textY+2),lines[i],(0,0,0),font=font)
+        draw.text((textX, textY),lines[i],(255,255,255),font=font)
+        lastY = textY
+
+
+    return img, draw
